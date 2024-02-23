2a112df02b9a28ba35372b8c6d3dd92789276c7e
Abdullah-Omar2 <omarabdullah378@gmail.com>
2024-02-23 22:28:12 +0200

    modifing task2 in week1

--- week1/task2/task2.c
+++ week1/task2/task2.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 void main (void)
 {
-	float f1,f2;
-	printf("before scanning: %f , %f\n",f1,f2);
-	printf("enter two float values\n");
+	float f1,f2,x;
+	printf("enter two float values\t");
 	scanf ("%f",&f1);
 	scanf ("%f",&f2);
-    printf("after scanning: %f , %f",f1,f2);
+	printf("before swapping: %f , %f\n",f1,f2);
+	x=f1;
+	f1=f2;
+	f2=x;
+    printf("after swapping: %f , %f",f1,f2);
 }
